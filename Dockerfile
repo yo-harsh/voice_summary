75cd4202445620df2b71d4ce0454fe010f7e2cb2
data-0101 <yo_harsh@outlook.com>
2023-09-29 22:17:36 +0530

    Added drf-spectacular for api docs

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.9-alpine3.13
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /tmp/requirements.txt
-COPY /requirements.dev.txt /tmp/requirements.dev.txt
+COPY ./requirements.dev.txt /tmp/requirements.dev.txt
 COPY ./app /app
 WORKDIR /app
 EXPOSE 8000
--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'voiceT',
+    'rest_framework',
+    'drf-spectacular',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
